4c0a2b5ffa2fc0a7d1da6d298fb7af4df5e10fa2
ryo <ba068082@gmail.com>
2019-02-17 14:44:48 +0900

    add: admin

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,15 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
+
+
+  validates :name, presence: true, length: {maximum: 30}
+  validate :validate_name_not_include_comma
+
+  def set_nameless_name
+    self.name = '名前なし' if name.blank?
+  end
+
+  def validate_name_not_include_comma
+    errors.add(:name, 'にカンマを含めることは出来ません') if name&.include?(',')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resource :users
+  end
   resources :tasks
   root to: 'tasks#index'
 end
