af81222a99ce02f525065612143579673e102bce
ryo <ba068082@gmail.com>
2019-02-17 21:30:32 +0900

    詳細画面のspec追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,23 +1,27 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
-  describe '一覧表示機能' do
-    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
-    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+  let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
 
-    before do
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-      visit login_path
-      fill_in 'session[email]', with: login_user.email
-      fill_in 'session[password]', with: login_user.password
-      click_button 'ログインする'
-    end
+  before do
+    visit login_path
+    fill_in 'session[email]', with: login_user.email
+    fill_in 'session[password]', with: login_user.password
+    click_button 'ログインする'
+  end
+
+
+  shared_examples 'ユーザーAの作成したタスクが表示される' do
+    it { expect(page).to have_content '最初のタスク' }
+  end
+
+  describe '一覧表示機能' do
 
     context 'ユーザーAがログインしている時' do
       let(:login_user) { user_a }
-      it 'ユーザーAの作成したタスクが表示される' do
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAの作成したタスクが表示される'
     end
 
     context 'ユーザーBがログインしている時' do
@@ -26,7 +30,46 @@ describe 'タスク管理機能', type: :system do
         expect(page).not_to have_content '最初のタスク'
       end
     end
+  end
 
+  describe '詳細表示機能' do
+
+    context 'ユーザーAがログインしている時' do
+      let(:login_user) { user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it_behaves_like 'ユーザーAの作成したタスクが表示される'
+    end
+  end
+
+  describe '新規作成機能' do
+    let(:login_user) { user_a }
+
+    before do
+      visit new_task_path
+      fill_in 'task[name]', with: task_name
+      click_button '登録する'
+    end
+
+    context '名称を入力した時' do
+      let(:task_name) { '新規作成のテストを書く' }
+      it '正常に登録される' do
+        expect(page).to have_selector '.alert-success', text: task_name
+      end
+    end
+
+    context '新規作成で名称を入力しなかった時' do
+      let(:task_name) {''}
+
+      it 'エラーになる' do
+        within '#error_explanation' do
+          expect(page).to have_content 'Nameを入力してください'
+        end
+      end
+    end
   end
 
 end
